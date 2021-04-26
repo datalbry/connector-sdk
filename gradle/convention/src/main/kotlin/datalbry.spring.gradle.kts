a55186d464d6371c25e6332e62fd2e5950beb989
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-04-26 17:41:41 +0200

    [KC-T-48] Refactor publication to also provide SNAPSHOT release

--- gradle/convention/src/main/kotlin/datalbry.spring.gradle.kts
+++ gradle/convention/src/main/kotlin/datalbry.spring.gradle.kts
@@ -2,13 +2,13 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("datalbry.kotlin")
+    id("datalbry.publication")
     id("org.springframework.boot")
     id("io.spring.dependency-management")
     id("org.jetbrains.kotlin.plugin.allopen")
     id("org.jetbrains.kotlin.plugin.noarg")
     id("org.jetbrains.kotlin.plugin.spring")
-    id("maven-publish")
-    id("signing")
+
 }
 
 allOpen {
@@ -21,45 +21,6 @@ noArg {
     annotation("javax.persistence.Entity")
     annotation("javax.persistence.MappedSuperclass")
     annotation("javax.persistence.Embeddable")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            val group = project.group as String
-            artifactId = "${group.substringAfterLast(".")}-${project.name}"
-            from(components["java"])
-            pom {
-                name.set("Connector SDK")
-                description.set("The Connector SDK is a Software Development Kit which enables user to implement " +
-                        "an Alxndria Connector in no time")
-                url.set("https://github.com/datalbry/connector-sdk")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("datalbry")
-                        name.set("DataLbry")
-                        email.set("devops@datalbry.io")
-                    }
-                }
-                scm {
-                    connection.set("https://github.com/datalbry/connector-sdk.git")
-                    developerConnection.set("scm:git:ssh:git@github.com:datalbry/connector-sdk.git")
-                    url.set("https://github.com/datalbry/connector-sdk")
-                }
-            }
-        }
-    }
-}
-
-configure<SigningExtension> {
-    useGpgCmd()
-    sign(publishing.publications["mavenJava"])
 }
 
 // ===========================================================================================
