5912130b957852703430b886633f295d95f0dc71
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-09-13 19:02:40 +0200

    fix(versions): update kotlin to 1.5.30, udpate KSP to 1.5.21, update jib to 3.1.4

--- sdk-plugin/build.gradle.kts
+++ sdk-plugin/build.gradle.kts
@@ -21,8 +21,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:2.3.5.RELEASE")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.9.RELEASE")
-    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.4.32-1.0.0-alpha07")
-    implementation("gradle.plugin.com.google.cloud.tools:jib-gradle-plugin:3.0.0")
+    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.5.21-1.0.0-beta06")
+    implementation("gradle.plugin.com.google.cloud.tools:jib-gradle-plugin:3.1.4")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
 }
