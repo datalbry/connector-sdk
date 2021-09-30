77b724e626dfe340f4b2eb7d1967f87fbbbceae0
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-09-30 21:19:19 +0200

    fix(plugin): not using version

--- sdk-plugin/src/main/kotlin/io/datalbry/connector/plugin/setup/SetupVersionHandler.kt
+++ sdk-plugin/src/main/kotlin/io/datalbry/connector/plugin/setup/SetupVersionHandler.kt
@@ -1,24 +1,33 @@
 package io.datalbry.connector.plugin.setup
 
+import java.util.Properties
 import org.gradle.api.Project
 import org.gradle.api.tasks.WriteProperties
 import org.jetbrains.kotlin.util.removeSuffixIfPresent
 import org.jetbrains.kotlin.util.suffixIfNot
 
 fun Project.setupVersionHandler() {
-    val projectVersion = project.property("connectorVersion") as String
+    val projectVersion = project.version as String
     version = findVersion(projectVersion)
 
     tasks.register("writeVersion", WriteProperties::class.java) {
-        it.outputFile = project.rootProject.file("gradle.properties")
-        it.property("projectVersion", version)
+        val propertiesFile = project.rootProject.file("gradle.properties")
+        it.outputFile = propertiesFile
+        val properties = Properties()
+        properties.load(propertiesFile.inputStream())
+        properties.forEach { property -> it.property(property.key as String, property.value) }
+        it.property("version", version)
     }
 
     tasks.register("incrementVersion", WriteProperties::class.java) {
         val currentVersion = projectVersion.removeSuffixIfPresent("-SNAPSHOT").substringAfterLast(".")
         val newVersion = "${projectVersion.substringBeforeLast(".")}.${currentVersion + 1}"
-        it.outputFile = project.rootProject.file("gradle.properties")
-        it.property("projectVersion", newVersion)
+        val propertiesFile = project.rootProject.file("gradle.properties")
+        it.outputFile = propertiesFile
+        val properties = Properties()
+        properties.load(propertiesFile.inputStream())
+        properties.forEach { property -> it.property(property.key as String, property.value) }
+        it.property("version", newVersion)
     }
 }
 
