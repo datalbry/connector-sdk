16a6ab712991dd3cefb7dcc94a47742f3818cfe8
Zeljko Bekcic <bekciczeljko@gmail.com>
2021-10-28 16:36:20 +0200

    feat: first draft of static schema provider

--- sdk/src/main/kotlin/io/datalbry/connector/sdk/schema/static/StaticSchemaProvider.kt
+++ sdk/src/main/kotlin/io/datalbry/connector/sdk/schema/static/StaticSchemaProvider.kt
@@ -1,18 +1,31 @@
 package io.datalbry.connector.sdk.schema.static
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.datalbry.connector.sdk.schema.SchemaProvider
 import io.datalbry.precise.api.schema.Schema
+import org.springframework.stereotype.Component
+import java.io.File
 
 /**
  * TODO
  */
-class StaticSchemaProvider: SchemaProvider {
+@Component
+class StaticSchemaProvider : SchemaProvider {
 
     /**
      * TODO
      */
     override fun getSchema(): Schema {
-        TODO("Not yet implemented")
+
+        val resource = this::class.java.getResource(schemaPath)
+            ?: throw java.nio.file.NoSuchFileException("Schema JSON at $schemaPath is not available")
+
+        val objectMapper = ObjectMapper()
+
+        return objectMapper.readValue(File(resource.file), Schema::class.java)
     }
 
+    companion object {
+        const val schemaPath = "/META-INF/datalbry/schema.json"
+    }
 }
