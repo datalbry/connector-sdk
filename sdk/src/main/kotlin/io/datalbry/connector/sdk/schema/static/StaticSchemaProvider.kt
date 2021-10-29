4d5c29759a6c279a4b0b2957aa972191cdbf8434
Zeljko Bekcic <bekciczeljko@gmail.com>
2021-10-29 15:42:01 +0200

    fix: correct resource inside spring boot jar

--- sdk/src/main/kotlin/io/datalbry/connector/sdk/schema/static/StaticSchemaProvider.kt
+++ sdk/src/main/kotlin/io/datalbry/connector/sdk/schema/static/StaticSchemaProvider.kt
@@ -3,8 +3,17 @@ package io.datalbry.connector.sdk.schema.static
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.datalbry.connector.sdk.schema.SchemaProvider
 import io.datalbry.precise.api.schema.Schema
+import org.aspectj.weaver.tools.cache.SimpleCacheFactory.path
 import org.springframework.stereotype.Component
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
+import java.net.URISyntaxException
+import java.net.URL
+import java.util.*
+import java.util.jar.JarEntry
+import java.util.jar.JarFile
+
 
 /**
  * This [SchemaProvider] looks for the by Precise generated schema.json inside the JAR at /META-INF/datalbry/schema.json
@@ -20,15 +29,14 @@ class StaticSchemaProvider : SchemaProvider {
      */
     override fun getSchema(): Schema {
 
-        val resource = this::class.java.getResource(schemaPath)
-            ?: throw java.nio.file.NoSuchFileException("Schema JSON at $schemaPath is not present")
+        val inputStream = this::class.java.classLoader.getResourceAsStream("/$schemaPath")
+            ?: throw java.nio.file.NoSuchFileException("Schema JSON at /$schemaPath is not present")
 
         val objectMapper = ObjectMapper()
-
-        return objectMapper.readValue(File(resource.file), Schema::class.java)
+        return objectMapper.readValue(inputStream, Schema::class.java)
     }
 
     companion object {
-        const val schemaPath = "/META-INF/datalbry/schema.json"
+        const val schemaPath = "META-INF/datalbry/schema.json"
     }
 }
