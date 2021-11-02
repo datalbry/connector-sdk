0b877c9c41707223037bc85367ceab8924b2c5ce
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-11-02 18:16:29 +0100

    feat: make documentcrawlprocessor configurable

--- sdk-autoconfigure/src/main/kotlin/io/datalbry/connector/autoconfigure/GenericCrawlProcessorAutoConfiguration.kt
+++ sdk-autoconfigure/src/main/kotlin/io/datalbry/connector/autoconfigure/GenericCrawlProcessorAutoConfiguration.kt
@@ -1,19 +1,20 @@
 package io.datalbry.connector.autoconfigure
 
+import io.datalbry.connector.api.document.DocumentCrawlProcessor
 import io.datalbry.connector.api.document.DocumentProcessor
 import io.datalbry.connector.sdk.consumer.generic.GenericCrawlProcessor
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConditionalOnClass(GenericCrawlProcessor::class)
 open class GenericCrawlProcessorAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean(DocumentCrawlProcessor::class)
     open fun genericCrawlProcessor(
         crawlProcessors: Collection<DocumentProcessor<*, *>>
-    ): GenericCrawlProcessor {
+    ): DocumentCrawlProcessor {
         return GenericCrawlProcessor(crawlProcessors)
     }
 
