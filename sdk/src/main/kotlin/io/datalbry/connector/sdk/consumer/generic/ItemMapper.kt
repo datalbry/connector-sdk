25d82891b6beb7e9601c624d8f5b92a699073227
Zeljko Bekcic <zeljko.bekcic@datalbry.io>
2021-12-07 21:59:39 +0300

    docs: adding kdocs for item mapper

--- sdk/src/main/kotlin/io/datalbry/connector/sdk/consumer/generic/ItemMapper.kt
+++ sdk/src/main/kotlin/io/datalbry/connector/sdk/consumer/generic/ItemMapper.kt
@@ -1,14 +1,36 @@
 package io.datalbry.connector.sdk.consumer.generic
 
-import io.datalbry.precise.api.schema.document.Document
 import io.datalbry.connector.api.DocumentEdge
+import io.datalbry.precise.api.schema.document.Document
 
+/**
+ * The [ItemMapper] creates a Document from a given type T.
+ */
 interface ItemMapper<T> {
 
+    /**
+     * The [getDocuments] method returns documents from the given item.
+     *
+     * @param item: Object which will be mapped to a document
+     * @return A collection of documents
+     */
     fun getDocuments(item: T): Collection<Document>
 
+    /**
+     * The [getDocuments] method returns the document edges from a given item.
+     *
+     * @param item: Object which will be mapped to a document
+     * @return A collection of document edges
+     */
     fun getEdges(item: T): Collection<DocumentEdge>
 
+    /**
+     * The [supports] method check if the item is supported. If an item is supported by an item mapper can execute the
+     * [getDocuments] and [getEdges] methods on the item.
+     *
+     * @param item: Object of some type which will be checked if supported
+     * @return true if supported, false otherwise.
+     */
     fun supports(item: T): Boolean
 
 }
