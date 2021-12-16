196890cda7f5b17f0f80965aea98808c2079144b
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-12-16 16:36:06 +0100

    ci: add support for semver plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
+    id("io.datalbry.plugin.semver") version "0.2.2"
     id("datalbry.publish-maven-central")
-    id("datalbry.version")
+    id("datalbry.kotlin")
     idea
 }
 
@@ -9,6 +10,11 @@ subprojects {
         mavenCentral()
         mavenLocal()
     }
+}
+
+semver {
+    version("alpha", "alpha.{COMMIT_TIMESTAMP}")
+    version("beta", "beta.{COMMIT_TIMESTAMP}")
 }
 
 group = "io.datalbry.connector"
