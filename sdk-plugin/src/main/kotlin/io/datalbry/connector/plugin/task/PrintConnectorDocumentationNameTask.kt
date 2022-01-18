9cec73eec048409eed7c03fed1fea9296fa695ac
Zeljko Bekcic <zeljko.bekcic@datalbry.io>
2022-01-18 17:26:49 +0100

    fix: joining with correct separator

--- sdk-plugin/src/main/kotlin/io/datalbry/connector/plugin/task/PrintConnectorDocumentationNameTask.kt
+++ sdk-plugin/src/main/kotlin/io/datalbry/connector/plugin/task/PrintConnectorDocumentationNameTask.kt
@@ -8,15 +8,15 @@ import org.jetbrains.kotlin.util.prefixIfNot
 /**
  * A simple gradle task which prints the connector name for the documentation at docs.datalbry.io.
  *
- * If the product name and vendor is the same the output will be: "${productName}-connector"
- * If they are different the output will be "${vendor}-${productName}-connector"
+ * If the product name and vendor is the same the output will be: "${productName} connector"
+ * If they are different the output will be "${vendor} ${productName} connector"
  */
 open class PrintConnectorDocumentationNameTask : DefaultTask() {
 
     @TaskAction
     fun printConnectorDocumentationNameTask() {
         val extension = project.extensions.getByType(ConnectorPluginExtension::class.java)
-        val name = "${extension.productName}-connector".prefixIfNot("${extension.vendor}-")
+        val name = listOf(extension.vendor, extension.productName, "connector").distinct().joinToString(" ")
         println(name)
     }
 
